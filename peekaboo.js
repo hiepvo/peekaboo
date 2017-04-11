d8d6e70eb5f32a936f55ae0370c7376602d3a2eb
hiep.vo <vanhiep1086@gmail.com>
2017-04-11 13:53:30 -0400

    change duration

--- peekaboo.js
+++ peekaboo.js
@@ -5,16 +5,14 @@
   var init        = {};
   var text       = document.querySelector('.evo_c-peekaboo__text');
   text.addEventListener('mouseleave', reveal, false);
-
-
   function reveal(el){
     if(this.tagName.toLowerCase() === 'div'){
       var span = document.querySelector('.evo_c-peekaboo__text' + '>span');
-      span.style.transitionDelay = '0.65s';
-      setTimeout(function(){
-        span.style.transitionDelay = '0s';
-      }, 100);
+      span.style.transitionDelay = '0.5s';
     }
+    setTimeout(function(){
+      span.style.transitionDelay = '0s';
+    }, 100);
   }
   /*-------------------------------*/
 
